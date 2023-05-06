68dfecc299d7e75613dcb5c848561c945c179937
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-05-05 17:53:20 -0700

    Add basic structure to the Hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,41 +1,22 @@
 import React from "react";
+import mrJam from "../assets/mrJam.png";
 
 const Hero = () => {
   return (
-    <>
-      <header>
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              to="/"
-              className="text-red-500 text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              Jam Web Dev
-            </a>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none"
-            }
-            id="example-navbar-warning"
-          >
-            <ul className="flex gap-5 list-none lg:ml-auto">
-              <li className="flex items-center">About Us</li>
-              <li className="flex items-center">Services</li>
+    <header className="header container mx-auto items-center flex h-screen max-h-860-px">
+      <div className="pr-8">
+        <h2 className="font-semibold text-4xl text-gray-800">
+          Create Awesome Web App
+        </h2>
+        <p className="mt-4 text-lg leading-relaxed text-gray-600">
+          Notus React is Free and Open Source. It does not change any of the CSS
+          from . It features multiple HTML elements and it comes with dynamic
+          components for ReactJS, Vue and Angular.
+        </p>
+      </div>
 
-              <li className="flex items-center">
-                <button
-                  className="bg-primary flex items-center gap-2 text-white font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
-                  type="button"
-                >
-                  <BiEnvelope /> Contact
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
-    </>
+      <img src={mrJam} alt="Character Mr.Jam illustration" />
+    </header>
   );
 };
 
