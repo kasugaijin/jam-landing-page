e806ce68da44c0cd30b32f6a3b17caaca7f28801
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-05-05 20:16:42 -0700

    Update Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <header className="header container mx-auto items-center flex h-screen max-h-860-px">
       <div className="pr-8">
-        <h2 className="font-semibold text-4xl text-gray-800">
+        <h2 className="font-semibold text-8xl text-gray-800">
           Create Awesome Web App
         </h2>
-        <p className="mt-4 text-lg leading-relaxed text-gray-600">
+        <p className="mt-8 text-3xl leading-relaxed text-gray-600">
           Notus React is Free and Open Source. It does not change any of the CSS
           from . It features multiple HTML elements and it comes with dynamic
           components for ReactJS, Vue and Angular.
