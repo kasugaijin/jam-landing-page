fe77272237ac543b3d81567ac93913bb311de1d5
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-05-05 18:03:17 -0700

    Add basic structure to AboutUs component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <section className="pt-20 pb-40 relative bg-primary">
+      <div
+        className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+        style={{ transform: "translateZ(0)" }}
+      >
+        <svg
+          className="absolute bottom-0 overflow-hidden"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon
+            className="text-primary fill-current"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
+      <div className="container mx-auto text-6xl text-white">
+        <h3>About Us</h3>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
