7e46c01dbbc8642bc8e0369fa2d0f001e311936b
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-05-09 13:42:30 -0700

    Add a hamburger menu and update styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,61 @@
 import React from "react";
 import { BiEnvelope } from "react-icons/bi";
+import { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
 
-export default function Navbar(props) {
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              to="/"
-              className="text-red-500 text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            >
-              Ruby Jam
-            </a>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none"
-            }
-            id="example-navbar-warning"
-          >
-            <ul className="flex gap-5 list-none lg:ml-auto">
-              <li className="flex items-center text-gray-800">About Us</li>
-              <li className="flex items-center text-gray-800">Services</li>
+    <nav className="top-0 fixed z-50 w-full py-3 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* logo*/}
+        <a
+          className="text-red-500 text-xl font-bold leading-relaxed inline-block py-2 uppercase"
+          href="/"
+        >
+          Ruby Jam
+        </a>
 
-              <li className="flex items-center">
-                <button className="btn-primary" type="button">
-                  <BiEnvelope /> Contact
-                </button>
-              </li>
-            </ul>
-          </div>
+        {/* menu links */}
+        <div className="hidden md:flex gap-5 items-center bg-white">
+          <a href="A" className=" text-gray-800 hover:text-primary">
+            About Us
+          </a>
+          <a href="A" className=" text-gray-800 hover:text-primary">
+            Services
+          </a>
+          <button className="btn-primary flex items-center" type="button">
+            <BiEnvelope /> Contact
+          </button>
         </div>
-      </nav>
-    </>
+
+        {/* Hamburger button */}
+        <button
+          className="block md:hidden text-primary hover:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-white"
+          aria-expanded={isOpen}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <IoMdMenu className="md:hidden w-8 h-8" />
+        </button>
+      </div>
+      {/* mobile menu */}
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:hidden container mx-auto flex flex-col items-end gap-5 bg-white`}
+      >
+        <a href="a" className=" text-gray-800 hover:text-primary">
+          About Us
+        </a>
+        <a href="a" className=" text-gray-800 hover:text-primary">
+          Services
+        </a>
+        <button className="btn-primary flex items-center" type="button">
+          <BiEnvelope /> Contact
+        </button>
+      </div>
+    </nav>
   );
 }
