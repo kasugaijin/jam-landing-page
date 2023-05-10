93fcf5ecad47d8979b7420941c55962accd94817
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-05-10 15:12:28 -0700

    Create useIntersectionObserver hook

---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,32 @@
+import { useRef, useEffect } from "react";
+
+function useIntersectionObserver(options) {
+  const ref = useRef(null);
+
+  const handleIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animation-show");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    // return () => {
+    //   if (ref.current) {
+    //     observer.unobserve(ref.current);
+    //   }
+    // };
+  }, [ref, options]);
+
+  return ref;
+}
+
+export default useIntersectionObserver;
