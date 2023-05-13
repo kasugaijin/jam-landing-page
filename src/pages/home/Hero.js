ff18087aa07c7193aecf2d4d596fd2a809a9e7cd
Ben R <robinsonbena@gmail.com>
2023-05-13 14:25:19 -0700

    update hero

--- src/pages/home/Hero.js
+++ src/pages/home/Hero.js
@@ -12,12 +12,11 @@ const Hero = () => {
     <header className="header container mx-auto items-center flex h-screen max-h-860-px relative">
       <div className="md:pr-8">
         <h2 className="font-semibold text-6xl text-gray-800">
-          Custom web solutions designed just for you
+        We'll bring your online vision to life.
         </h2>
         <p className="mt-8 text-xl leading-relaxed text-gray-600">
           From concept to care, we build and maintain custom websites and
-          full-stack applications specialized in React and Ruby on Rails. We'll
-          bring your online vision to life and keep it running smoothly.
+          full-stack applications.
         </p>
       </div>
 
