626601001c66a0ddfb9b337de0770a5e2fa23034
Ben R <robinsonbena@gmail.com>
2023-05-13 20:49:55 -0700

    refactor cta button into own component for reuse

---
+++ src/components/CtaButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BiEnvelope } from "react-icons/bi";
+
+const CtaButton = () => {
+  return (
+    <button className="btn-primary flex items-center ml-auto" type="button">
+          <BiEnvelope /> Contact
+    </button>
+  )
+}
+
+export default CtaButton
