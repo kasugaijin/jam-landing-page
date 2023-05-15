1ec662117822300b22aa8b65c24a9886967ce50b
Ben R <robinsonbena@gmail.com>
2023-05-14 17:03:18 -0700

    add scroll to contact form function on component

--- src/components/CtaButton.js
+++ src/components/CtaButton.js
@@ -2,11 +2,26 @@ import React from 'react'
 import { BiEnvelope } from "react-icons/bi";
 
 const CtaButton = () => {
+  const scrollToContactForm = () => {
+    const contactFormElement = document.getElementById('contact-form');
+    if (contactFormElement) {
+      const { top } = contactFormElement.getBoundingClientRect();
+      window.scrollTo({
+        top: window.pageYOffset + top,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <button className="btn-primary flex items-center ml-auto" type="button">
-          <BiEnvelope /> Contact
+    <button 
+      className="btn-primary flex items-center ml-auto" 
+      type="button"
+      onClick={scrollToContactForm}
+    >
+      <BiEnvelope /> Contact
     </button>
-  )
-}
+  );
+};
 
 export default CtaButton
